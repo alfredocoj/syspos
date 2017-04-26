67176a7685eaafe07f3f618bccf778a6fcd1414f
Alfredo Oliveira <alfredo.coj@gmail.com>
2017-04-26 00:34:32 -0300

    bd configuraction

--- posys/settings.py
+++ posys/settings.py
@@ -15,10 +15,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'posys',                                         # Or path to database file if using sqlite3.
+        'NAME': 'syspos',                                         # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': '123456',
+        'USER': 'phpmyadmin',
+        'PASSWORD': 'some_pass',
         'HOST': 'localhost',             # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
